e0346e5cdd134af106e7f075a4aecbc0b9753253
dsenf <70286386+dsenf@users.noreply.github.com>
2020-10-25 20:12:47 -0500

    made a change

---
+++ js/app.js
@@ -0,0 +1,33 @@
+
+
+
+// Create a variable addToHead and use the querySelector method to assign the ‘head’ element to the addToHead variableaddToHead variable and use querySelector to get the CSS head element
+var = addToHead = document.querySelector('head');
+
+// Create a ‘link’ element that will be assigned to your cssLink variable and store it in a variable.
+var cssLink = document.createElement('link');
+
+// Use the appendChild method on the addToHead variable to add cssLink to the DOM
+
+addhead() = addToHead.appendChild(cssLink);
+
+// With your cssLink variable, use the setAttribute method to pass in ‘rel’, ‘stylesheet’ as arguments
+
+
+
+
+
+// Repeat the previous step with ‘href’, ‘css/style.css’ as arguments
+
+// Create a variable wrapper and use the createElement method to create a ‘div’ that will be assigned to the wrapper variable
+
+// Create a variable bodY and use the querySelector method to assign the ‘body’ element to the bodY variable
+
+// Use the appendChild method on the bodY variable to add wrapper to the DOM.
+
+
+// 
+
+
+
+
