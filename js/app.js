491932a80dee9d8f029c27b8716fd6db95429525
dsenf <70286386+dsenf@users.noreply.github.com>
2020-10-27 19:25:05 -0500

    change

--- js/app.js
+++ js/app.js
@@ -34,9 +34,9 @@ bodY.appendChild(wrapper);
 //Use the appendChild method on the wrapper variable to add divs to the DOM. (2.5 points)
 //Use the classList method to add the class name ‘wrapperDivs’ to your divs variable.
 
-
+/*
 for (i = 0; i < 4; i++) {
   var divs = document.createElement('div');
   div.appendChild(divs);
- 
+ */
 
