31ba8f7c0cc183b5ef3fbb8a4c8c6cdd6b0271dc
dsenf <70286386+dsenf@users.noreply.github.com>
2020-10-27 19:44:38 -0500

    f

--- js/app.js
+++ js/app.js
@@ -37,7 +37,8 @@ bodY.appendChild(wrapper);
 
 for (i = 0; i < 4; i++) {
   var divs = document.createElement('div');
-  div.classList.add("divs");  
+  div.appendChild(divs);
+  
 }
 /*
   div.appendChild(divs);
